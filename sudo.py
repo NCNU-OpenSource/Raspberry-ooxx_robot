5aac4f8b7a09b6bb8968dc582474e270147e4fc3
tommygood <96759292+tommygood@users.noreply.github.com>
2023-01-04 21:44:50 +0800

    Update sudo.py

--- sudo.py
+++ sudo.py
@@ -1,26 +1,22 @@
+# this file is for find the O and return position in 3x3 square map
 import cv2
 import numpy as np
 from imutils.object_detection import non_max_suppression
 import argparse
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+ap.add_argument("-i", "--image", required = True, help = "Path to the image") # get image path from execute.py
 args = vars(ap.parse_args())
 # load the image, clone it for output, and then convert it to grayscale
 img = cv2.imread(args["image"])
-x_row = [[600,850], [851,1080], [1081,1350]]
+# each range of (x,y) in the 9(3x3) frame
+x_row = [[600,850], [851,1080], [1081,1350]] 
 y_row = [[300,450], [451,570], [571,850]]
-
-# Reading the image and the template
-#img = cv2.imread('sta_circle.jpg')
-#temp = cv2.imread('o_template_1.png')
-
-# save the image dimensions
-
 
 # Define a minimum threshold
 thresh = 0.4
 total_box = []
+# use for check different templates' frames with different colors
 global color_index
 color_index = 0
 def circleBox(temp, color_index) :
@@ -56,7 +52,8 @@ def circleBox(temp, color_index) :
     #print(len(boxes))
     index = 0
     no_lap = 0
-    for (x1, y1, x2, y2) in boxes:
+    for (x1, y1, x2, y2) in boxes: # each detected object
+        # for debug
         #cv2.rectangle(img, (x1-50, y1-50), (x2-50, y2-50),
         #                (255, color_index * 63, 0), 1)
         #print("x = " + str((x1+x2)/2),end = '')
@@ -80,7 +77,7 @@ def circleBox(temp, color_index) :
     cv2.imwrite(args['image'], img)
     #print("總共偵測到 %d 個圓形" %index)
 
-def checkInclude(check_list, index, boxes) :
+def checkInclude(check_list, index, boxes) : # 檢查是否有重疊物件
     n_index = 0
     #if len(boxes) == 0 :
     #    return False
@@ -96,47 +93,12 @@ def checkInclude(check_list, index, boxes) :
                 return True
             if (x1 >= check_list[0] and x2 <= check_list[2]) and (y1 >= check_list[1] and y2 <= check_list[3]):
                 return True
-            
-           # n_index += 1
-       # else : # 比要檢查的還要後面，所以不要檢查
-           # n_index += 1
-            #continue
-        """while index > n_index:
-            if check_list[0] < x1 and check_list[2] > x1:
-                return True
-            if check_list[0] < x2 and check_list[2] > x2:
-                return True
-            if check_list[1] < y1 and check_list[3] > y1:
-                return True
-            if check_list[1] < y2 and check_list[3] > y2:
-                return True
-            if check_list[0] < x1 and check_list[2] > x2:
-                return True
-            if check_list[1] < y1 and check_list[3] > y2:
-                return True
-
-            n_index += 1"""
-        """if index > n_index : # 比要檢查的還要前面，所以要檢查
-            if x2 >= check_list[0] and x1 <= check_list[0] : # 檢查 x1
-                return True
-            if x2 >= check_list[2] and x1 <= check_list[2] : # 檢查 x2
-                return True
-            if y2 >= check_list[1] and y1 <= check_list[1] : # 檢查 x0
-                return True
-            if y2 >= check_list[3] and y1 <= check_list[3] : # 檢查 x0
-                return True"""
         n_index += 1
-    #print(img)
     cv2.rectangle(img, (check_list[0]-50, check_list[1]-50), (check_list[2]-50, check_list[3]-50),
                     (255, color_index * 63, 0), 1)
     return False
 
-#circleBox(cv2.imread('o_template_1.png'))
-#circleBox(cv2.imread('x_template_1.png'))
-#circleBox(cv2.imread('x_template_2.jpg'))
-#circleBox(cv2.imread('circle_test_6.jpg'))
-
-
+# 可以用不同的模板
 #color_index += 1
 #circleBox(cv2.imread('templates/5_5_temp_4.jpg'), color_index)
 #color_index += 1
@@ -145,13 +107,6 @@ def checkInclude(check_list, index, boxes) :
 #circleBox(cv2.imread('templates/5_5_temp_8.jpg'), color_index)
 color_index += 1
 circleBox(cv2.imread('templates/5_5_temp_9.jpg'), color_index)
-
-"""###
-
-for i in range(1,len(new_total_box)):
-    for j in range(i,len(new_total_box)):
-
-###"""
 
 nn = 0
 #print("總共有 " + str(len(total_box)))
@@ -171,7 +126,8 @@ for i in total_box :
         x_index += 1
 
     #print(str(nn) + "號。範圍 " + str(i) + ", 中心 : " + str((i[2]+i[0])/2) + ", " + str((i[3] + i[1])/2))
-new_total_box = []
+# 當要用多個模板再用
+"""new_total_box = []
 index_n = 0
 for i in total_box :
     #cv2.rectangle(img, (x1-50, y1-50), (x2-50, y2-50),
@@ -180,11 +136,8 @@ for i in total_box :
         #print(i)
         #cv2.rectangle(img, (i[0], i[1]), (i[2], i[3]),(255, 255, 0), 1)
         new_total_box.append(i)
-    index_n += 1
+    index_n += 1"""
 cv2.imwrite(args['image'], img)
-#print((new_total_box))
-#print(len(new_total_box))
-#circleBox(cv2.imread('circle_game_temp1.jpg'))
 
 # Show the template and the final output
 #cv2.imshow("Template", temp)
